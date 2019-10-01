48c5b56fbc250be1303a3a60c63454221f01ca37
Daniel-potros <56022189+Daniel-potros@users.noreply.github.com>
2019-10-01 02:28:24 -0700

    Add files via upload

---
+++ Timbiriche/src/Juego/Cuadrado.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+import java.awt.Shape;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author User 1
+ */
+public class Cuadrado extends Figura{
+private Color color;
+    
+    
+    @Override
+    public void setColor(Color color) {
+        
+    }
+
+    @Override
+    public void Dibujar(Shape figura) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
---
+++ Timbiriche/src/Juego/Figura.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+
+/**
+ *
+ * @author User 1
+ */
+public abstract class Figura  {
+    private Color color;
+    private Shape figura;
+    
+    
+    public abstract void Dibujar(Shape figura);
+    
+    public abstract void setColor(Color color);
+    
+}
